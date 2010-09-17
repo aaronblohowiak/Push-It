0dcd36aa5fb6fe0c9c23aaa3b4c122576c40aa94
Aaron Blohowiak <aaron.blohowiak@gmail.com>
2010-09-17 09:41:57 -0700

    uuid test

---
+++ server/test/uuid_test.js
@@ -0,0 +1,31 @@
+var test = {},
+    uuid = require("uuid").uuid;
+    
+module.exports = test;
+
+test["uuid should return only 0-9a-zA-Z\-_ so we can have url-safe uuids"] = function(assert){
+  var guid = uuid(),
+      validChars = guid.match(/[\da-zA-Z\-_]/g).length,
+      len = guid.length;
+  assert.equal(validChars, len, guid + " has "+validChars+" when it should have "+len );
+};
+
+test["uuid should return 22 chars by default"] = function(assert){
+  var guid = uuid(),
+      len = guid.length;
+  assert.equal(len, 22, guid + " should be 22 chars long, it was "+len);
+};
+
+test["uuid should respect a length param"] = function(assert){
+  var guid = uuid(10);
+  assert.equal(guid.length, 10, guid + "should be 10 chars long");
+};
+
+test["uuid should be respect a 'base' param"] = function(assert){
+  var guid = uuid(100, 36),
+      validChars = guid.match(/[\dA-Z]/g).length,
+      len = guid.length;
+  
+  assert.equal(validChars, len, guid + " has "+validChars+" when it should have "+len );
+};
+
